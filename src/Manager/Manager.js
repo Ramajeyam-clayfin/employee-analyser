ca74303a9dfa4db99b06793fb57fffd440069f42
Ramajeyam <jeyamjeyam6@gmail.com>
2022-02-22 21:04:05 +0530

    axios

--- src/Manager/Manager.js
+++ src/Manager/Manager.js
@@ -51,6 +51,7 @@ export default function Manager (){
             console.log('axios finished')
           })
           
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       },[]);
       
       if (loading) {
